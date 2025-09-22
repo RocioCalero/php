161dbb1d522ed19123e493b27665664a080f1639
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-22 10:28:58 +0200

    Ejercicio 2

---
+++ actividades/ejercicio2.php
@@ -0,0 +1,6 @@
+<?php
+	$nombre = "Rocio";
+	$direccion = "C/ Peñaflor n 6";
+	$numero = "123456789";
+	echo "Mi nombre es $nombre"."<br>Mi dirección es $direccion"."<br>Y mi numero es $numero";
+?>
