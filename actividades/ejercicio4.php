0d08d1ca6f11659855e43190b816f3de60567b0c
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-22 10:49:33 +0200

    Ejercicio 4 y Saludo

---
+++ actividades/ejercicio4.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    Introduce tu nombre:
+    <form action="saludo.php" method="get">
+        <input type="text" name="nombre"><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ actividades/saludo.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+    Hola <?php 
+    echo $GET['nombre'];
+    ?> que tengas un buen dia.
+</body>
+</html>
