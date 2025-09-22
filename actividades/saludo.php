ca64e710e119fc7c40b832550ccfb57df3a04396
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-22 10:57:23 +0200

    Saludo

--- actividades/saludo.php
+++ actividades/saludo.php
@@ -2,8 +2,8 @@
 <head>
 </head>
 <body>
-    Hola <?php 
-    echo $GET['nombre'];
-    ?> que tengas un buen dia.
+    <?php 
+    echo "Hola ". $GET['nombre'] ." que tengas un buen dia.";
+    ?> 
 </body>
 </html>
