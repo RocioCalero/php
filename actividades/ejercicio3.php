695848d75258cdf50a04d0d34910605e91e397df
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-22 10:36:18 +0200

    Ejercicio 3

---
+++ actividades/ejercicio3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <table>
+        <?php
+        $uno = "uno";
+        $one = "one";
+            echo "<tr><td>$uno</td><td>$one</td></tr>";
+        ?>
+    </table>
+</body>
+</html>
