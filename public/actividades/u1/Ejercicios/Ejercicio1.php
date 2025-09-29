9b146acc3369f8a81736bbebc660e2bb69954a34
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-29 10:06:43 +0200

    Actividad 1

---
+++ public/actividades/u1/Ejercicios/Ejercicio1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio1</title>
+</head>
+<body>
+    <?php
+    for ($i = 1; $i <= 100; $i++){
+        if($i % 5 == 0){
+            echo "$i". " es múltiplo de 5 <br>";
+        }
+    }
+    ?>
+</body>
+</html>
