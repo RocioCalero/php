a82b10bc0d1855fe508ad4ff88c6e5d46892c6b4
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-29 10:22:37 +0200

    Actividad 3

--- public/actividades/u1/Ejercicios/Ejercicio3.php
+++ public/actividades/u1/Ejercicios/Ejercicio3.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio2</title>
+    <title>Ejercicio3</title>
 </head>
 <body>
     <?php
-        $i = 0;
         echo "<h1>Multiplos de 5</h1>";
-        while ($i<=100){
+        $i = 0;
+        do {
             if ($i % 5 == 0){
-                echo "$i". " es múltiplo de 5 <br>";
+            echo "$i". " es múltiplo de 5 <br>";
             }
-            $i++;
-        }
+            $i++; // Incrementamos el contador
+        } while ($i <= 100);
     ?>
 </body>
 </html>
