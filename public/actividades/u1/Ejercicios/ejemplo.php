2fb72e53f0529896cd5e6aa0378f7e7ecc47e998
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-09-29 09:51:59 +0200

    Estructura basica

---
+++ public/actividades/u1/Ejercicios/ejemplo.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo</title>
+</head>
+<body>
+    <?php
+        for ($i = 0; $i < 10; $i++){
+            echo "El valor i es " .$i. "<br>";
+        }
+    ?>
+</body>
+</html>
